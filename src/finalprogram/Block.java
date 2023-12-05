0e0a9101f05877ba2c7a826bfb8a9379d6d38d80
think-v2 <146667018+think-v2@users.noreply.github.com>
2023-12-05 12:52:43 -0800

    Update Block.java

--- src/finalprogram/Block.java
+++ src/finalprogram/Block.java
@@ -4,21 +4,14 @@
 * class: CS 4450 Computer Graphics
 *
 * assignment: final program
-* date last modified: 11/7/2023
+* date last modified: 12/5/2023
 *
 * purpose: This holds block data
 *
 ****************************************************************/
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package finalprogram;
 
-/**
- *
- * @author Aidan
- */
 public class Block{
     private int blockID;
     private int chunkID;
